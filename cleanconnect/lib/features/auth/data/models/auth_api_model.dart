5b1be051130417576589e36cc3a34c103af1f718
Yubraj Karki <yubrajkarki188@gmail.com>
2026-01-15 16:29:17 +0545

    changes in api model

--- cleanconnect/lib/features/auth/data/models/auth_api_model.dart
+++ cleanconnect/lib/features/auth/data/models/auth_api_model.dart
@@ -1,6 +1,9 @@
 
 
+import 'package:cleanconnect/features/auth/domain/entities/auth_entity.dart';
+
 class AuthApiModel {
+  final String? id;
   final String fullName;
   final String email;
   final String? phoneNumber;
@@ -9,6 +12,7 @@ class AuthApiModel {
   final String? profilePicture;
 
   AuthApiModel({
+    this.id,
     required this.fullName,
     required this.email,
     this.password,
@@ -32,6 +36,7 @@ class AuthApiModel {
   //from JSON 
   factory AuthApiModel.fromJson(Map<String, dynamic> json) {
     return AuthApiModel(
+      id: json['id'] ?? json['_id'],
       fullName: json['name'] as String,
       email: json['email'] as String,
       phoneNumber: json['phoneNumber'] as String?,
@@ -40,5 +45,34 @@ class AuthApiModel {
       profilePicture: json['profilePicture'] as String?,
     );
   }
+
+  // toEntity
+  AuthEntity toEntity(){
+    return AuthEntity(
+      authId: id,
+      fullName: fullName,
+      email: email,
+      phoneNumber: phoneNumber,
+      address: address,
+      profilePicture: profilePicture,
+    );
+  }
   
+  // fromEntity
+  factory AuthApiModel.fromEntity(AuthEntity entity){
+    return AuthApiModel(
+      fullName: entity.fullName,
+      email: entity.email,
+      phoneNumber: entity.phoneNumber,
+      address: entity.address,
+      password: entity.password,
+      profilePicture: entity.profilePicture,
+    );
+  }
+
+  // toEntityList
+  static List<AuthEntity> toEntityList(List<AuthApiModel> models){
+    return models.map((model) => model.toEntity()).toList();
+  }
+
 }
--- cleanconnect/lib/features/auth/domain/entities/auth_entity.dart
+++ cleanconnect/lib/features/auth/domain/entities/auth_entity.dart
@@ -6,6 +6,7 @@ class AuthEntity extends Equatable {
   final String fullName;
   final String email;
   final String? phoneNumber;
+  final String address;
   final String? password;
   final CustomerEntity? customer;
   final String? profilePicture;
@@ -15,9 +16,10 @@ class AuthEntity extends Equatable {
     required this.fullName,
     required this.email,
     this.phoneNumber,
+    required this.address,
     this.password,
     this.customer,
-    this.profilePicture, required String address,
+    this.profilePicture,
   });
 
   @override
@@ -26,6 +28,7 @@ class AuthEntity extends Equatable {
     fullName,
     email,
     phoneNumber,
+    address,
     password,
     customer,
     profilePicture,
